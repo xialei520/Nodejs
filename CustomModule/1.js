c63e9827eb025a504b26b070bcf29ea8b6ca8cdc
xialei <377284071@qq.com>
2018-08-22 23:33:35 +0800

    8.22发布自定义模块

---
+++ CustomModule/1.js
@@ -0,0 +1,4 @@
+var mod = require('./mod.js');
+const xialei = require('xialeitest')
+console.log(mod.a, mod.b, mod.c);
+console.log(xialei.sum(1,2,3,4,5,6,7,8,9))
---
+++ CustomModule/mod.js
@@ -0,0 +1,8 @@
+var a = 1;
+
+// exports.a = 1;
+module.exports = {
+    a: 1,
+    b: 3,
+    c: 6
+}
