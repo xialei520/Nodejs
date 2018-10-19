d88b6928f7a6b0aa746f0027b78059c61ef960b1
xialei <377284071@qq.com>
2018-10-20 00:06:31 +0800

    10.20

--- wangyiyun/server.js
+++ wangyiyun/server.js
@@ -1,16 +1,18 @@
 const request = require('superagent');
 const cheerio = require('cheerio');
 const fs = require('fs-extra');
-const puppeteer = require('puppeteer');
+ 
 
 const path = require('path');
-const url = 'https://music.163.com/#/playlist?id=115218242';
+// 下载网易云热歌榜前200首歌
+const url = 'https://music.163.com/discover/toplist?id=3778678';
 
 async function getId(url){
     const data = await request.get(url);
    
     const $ = cheerio.load(data.text);
     
+    console.log($("#g_iframe tbody tr").length);
      
     
 }
