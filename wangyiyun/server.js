cb65cbaaec1ac135ac26ff4d0fb1516a93a7c810
xialei <377284071@qq.com>
2018-10-21 00:01:03 +0800

    10.11

--- wangyiyun/server.js
+++ wangyiyun/server.js
@@ -9,11 +9,8 @@ const url = 'https://music.163.com/discover/toplist?id=3778678';
 
 async function getId(url){
     const data = await request.get(url);
-   
     const $ = cheerio.load(data.text);
-    
     console.log($("#g_iframe tbody tr").length);
-     
     
 }
 getId(url);
