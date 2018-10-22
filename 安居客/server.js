7750dc43ee640310df83b83d5783720b95e9b838
xialei <377284071@qq.com>
2018-10-22 10:26:53 +0800

    10.22

--- 安居客/server.js
+++ 安居客/server.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra');
 const cheerio = require('cheerio');
 const request = require('superagent');
 const path = require('path');
+const xlsx = require('node-xlsx');
 
 const url = 'https://qd.zu.anjuke.com/fangyuan/px3/';
 
@@ -14,54 +15,64 @@ async function getPage(url) {
 
         if (i == 1) {
             var eachPage = 'https://qd.zu.anjuke.com/fangyuan/px3/';
-
         } else {
             var eachPage = 'https://qd.zu.anjuke.com/fangyuan/p' + i + '-px3/';
-
         }
         await getUrl(eachPage);
         // console.log(eachPage)
     }
 }
 async function getUrl(eachPage) {
-    const data = await request(url);
-    const $ = cheerio.load(data.text);
+    console.log(eachPage)
+    // const data = await request(eachPage);
+    // const $ = cheerio.load(data.text);
 
 
-    console.log($('#list-content .zu-itemmod').length, 'sss')
+    // console.log($('#list-content .zu-itemmod').length, 'sss')
+    // var obj = {
+    //     name: '第' + i + '页'
 
-    for (var j = 0; j < $('#list-content .zu-itemmod').length; j++) {
-        // console.log($('#list-content .zu-itemmod').eq(j).attr('link'));
-        var href = $('#list-content .zu-itemmod').eq(j).attr('link');
-        await load(href)
-    }
+    // };
+    // var data = [
+    //     [
+    //         '姓名', '价格', '户型', '面积', '朝向', '楼层', '装修', '类型', '小区'
+    //     ]
+    // ]
+    // for (var j = 0; j < $('#list-content .zu-itemmod').length; j++) {
+        
+    //     var href = $('#list-content .zu-itemmod').eq(j).attr('link');
+    //     // await load(href)
+    // }
 }
-async function load(href) {
+async function load(href, i) {
+   
     const message = await request(href);
     const $_ = cheerio.load(message.text);
-    var imgArr = [];
-    for (var k = 0; k < $_('#room_pic_wrap .img_wrap img').length; k++) {
-        imgArr.push($_('#room_pic_wrap .img_wrap img').eq(k).attr('src'));
-    }
-    var obj = {
-        img: imgArr,
-    }
 
+    for (var k = 0; k < $_('#room_pic_wrap .img_wrap img').length; k++) {
+        imgArr.push($_('#room_pic_wrap .img_wrap img').eq(k).attr('src'))
+    }
+    var each = [];
     console.log($_('.house-info-zufang li').length, "000");
 
-    obj.price = await $_('.house-info-zufang li').eq(0).find('.price').text();
+    var price = await $_('.house-info-zufang li').eq(0).find('.price').text();
     // console.log($_('.house-info-zufang li').eq(p).find('.price').text())
-    obj.huxing = await $_('.house-info-zufang li').eq(1).find('.info').text();
-    obj.mianji = await $_('.house-info-zufang li').eq(2).find('.info').text();
-    obj.chaoxiang = await $_('.house-info-zufang li').eq(3).find('.info').text();
-    obj.louceng = await $_('.house-info-zufang li').eq(4).find('.info').text();
-    obj.zhuangxiu = await $_('.house-info-zufang li').eq(5).find('.info').text();
-    obj.leixing = await $_('.house-info-zufang li').eq(6).find('.info').text();
-    obj.xiaoqu = await $_('.house-info-zufang li').eq(7).find('a').eq(0).text()+'('+ $_('.house-info-zufang li').eq(7).find('a').eq(1).text()+' '+$_('.house-info-zufang li').eq(7).find('a').eq(1).text()+')';
-    obj.name = await $_('.rbox').find('.broker-name').text();
-    arrData.push(obj);
+    var huxing = await $_('.house-info-zufang li').eq(1).find('.info').text();
+    var mianji = await $_('.house-info-zufang li').eq(2).find('.info').text();
+    var chaoxiang = await $_('.house-info-zufang li').eq(3).find('.info').text();
+    var louceng = await $_('.house-info-zufang li').eq(4).find('.info').text();
+    var zhuangxiu = await $_('.house-info-zufang li').eq(5).find('.info').text();
+    var leixing = await $_('.house-info-zufang li').eq(6).find('.info').text();
+    var xiaoqu = await $_('.house-info-zufang li').eq(7).find('a').eq(0).text() + '(' + $_('.house-info-zufang li').eq(7).find('a').eq(1).text() + ' ' + $_('.house-info-zufang li').eq(7).find('a').eq(1).text() + ')';
+    var name = await $_('.rbox').find('.broker-name').text();
+    each.push(name).push(price).push(huxing).push(mianji).push(chaoxiang).push(louceng).push(zhuangxiu).push(leixing).push(xiaoqu);
+    data.push(each);
+    obj.push(data);
+    arrData.push(obj)
     console.log(JSON.stringify(arrData))
-    // await fs.writeFile('租房信息.txt', JSON.stringify(obj), function(err){
-    //     console.log(err)
-    // })
+
 }
+var buffer = xlsx.build(arrData)
+fs.writeFile('租房信息.xlsx',  buffer, function (err) {
+    console.log(err)
+})
